c8a494d5723202da0d395505b35b1a211c5bb217
Agustín <108036702+Agus-DS@users.noreply.github.com>
2025-09-19 08:58:55 -0300

    Add files via upload

---
+++ guia4.hs
@@ -0,0 +1,40 @@
+-- Ejercicio 1
+
+fibonacci :: Int -> Int
+fibonacci x | x < 0 = undefined
+            | x == 0 = 0
+            | x == 1 = 1
+            | otherwise = fibonacci (x-1) + fibonacci (x-2)
+
+-- Ejercicio 2 
+parteEntera :: Float -> Integer
+parteEntera  x | x <= 1 = 0
+               |  otherwise = 1 + parteEntera(x-1)         
+
+-- Ejercicio 3
+esDivisible :: Integer -> Integer -> Bool
+esDivisible x y = resto x y == 0 
+  
+resto :: Integer -> Integer -> Integer
+resto x y | x < y = x
+          | otherwise = resto (x-y) y 
+
+-- ejercicio 4
+sumaImpares :: Integer -> Integer
+sumaImpares x | x == 1 = 1
+              | otherwise = (2 * x) - 1 + sumaImpares(x-1)
+
+-- ejercicio 5 (preguntar por el requiere con el caso de negativos)
+
+medioFact :: Integer -> Integer
+medioFact x | x == 0 = 1
+            | x < 0 = 1
+            | otherwise = x * medioFact(x-2)
+
+-- ejercicio 6
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales x | x < 10 && x >= 0 = True
+                      | mod x 10 /= mod (div x 10) 10  = False
+                      | otherwise = todosDigitosIguales (div x 10)
+
+-- ejercicio 7
